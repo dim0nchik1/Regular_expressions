1836984262576c7d765048397abb750f93d77679
Dim0nchik <dzhikon@bk.ru>
2025-03-05 21:40:29 +0300

    homework

--- src/__tests__/app.test.js
+++ src/__tests__/app.test.js
@@ -1,8 +1,22 @@
-import validator from "../js/app"
-
-
+import Validator from "../js/app"
 
 test("validator test", () => {
-    const str = validator.validateUsername("hello_123_world")
-    expect(str).toBe("true")
+    const str = Validator.validateUsername("hello_123_world")
+    expect(str).toBe(true)
 })
+
+test("not validators test", () => {
+    const str = Validator.validateUsername("1hello_123_world")
+    expect(str).toBe(false)
+})
+
+test("not validators test_1", () => {
+    const str = Validator.validateUsername("_hello_123_world_")
+    expect(str).toBe(false)
+})
+
+test("not validators test_2", () => {
+    const str = Validator.validateUsername("h-1_w")
+    expect(str).toBe(true)
+})
+
